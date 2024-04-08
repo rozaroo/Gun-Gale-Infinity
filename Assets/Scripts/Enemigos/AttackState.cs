4148f97b4f5327261e2565be706230c0332d8757
rozaroo <roramos@uade.edu.ar>
2024-04-08 11:53:46 -0300

    Enemigo en escena

--- Assets/Scripts/Enemigos/AttackState.cs
+++ Assets/Scripts/Enemigos/AttackState.cs
@@ -7,7 +7,7 @@ public class AttackState : StateMachineBehaviour
     Transform player;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
