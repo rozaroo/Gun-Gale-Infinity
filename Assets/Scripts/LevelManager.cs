a494513292104e96930a612b726b775c533f4bfc
rozaroo <roramos@uade.edu.ar>
2024-05-14 17:33:02 -0300

    Pantalla de derrota añadida

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public int Enemies;
     public GameObject Portal;
-
+    public int Nivel;
     [SerializeField] GameObject defeatScreen;
     PlayerController pController;
 
     private void Awake()
     {
-        pController = GetComponent<PlayerController>();
+        pController = GameObject.FindObjectOfType<PlayerController>();
+        if (pController == null) Debug.LogError("No ha sido encontrado");
     }
 
     void Start()
@@ -26,15 +28,29 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Y)) Restart();
         if (Enemies == 0) Portal.SetActive(true);
-        if (pController.Active == false) defeatScreen.SetActive(true);
+        if (pController != null) 
+        {
+            if (pController.Active != null)
+            {
+
+                if (pController.Active == false) 
+                {
+                    Time.timeScale = 0f;
+                    defeatScreen.SetActive(true); 
+                }
+                else Debug.Log("es null");
+            }
+        }
     }
+        
     public void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene());
+        SceneManager.LoadScene(Nivel);
     }
     public void MainMenu()
     {
-        SceneManager.LoadScene(SceneManager.LoadScene(0));
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -8,30 +8,26 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject levelsButtons;
+    //[SerializeField] private GameObject levelsButtons;
 
     public void QuitGame()
     {
         Application.Quit();
     }
-    public void ToggleCredits()
-    {
-        credits.SetActive(!credits.active);
-    }
     public void LoadLevel0()
     {
-        SceneManager.LoadScene(SceneManager.LoadScene(1));
+        SceneManager.LoadScene(1);
     }
     public void LoadLevel1()
     {
-        SceneManager.LoadScene(SceneManager.LoadScene(2));
+        SceneManager.LoadScene(2);
     }
     public void LoadLevel2()
     {
-        SceneManager.LoadScene(SceneManager.LoadScene(3));
+        SceneManager.LoadScene(3);
     }
-    public void ToggleLevels()
-    { 
-        levelsButtons.SetActive(!levelsButtons.active);
-    }
+    //public void ToggleLevels()
+    //{ 
+        //levelsButtons.SetActive(!levelsButtons.active);
+    //}
 }
