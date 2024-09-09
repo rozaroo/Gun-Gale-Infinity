e578e29232bfbfb562d88b87ffaa9e8fbb0b2140
rozaroo <roramos@uade.edu.ar>
2024-09-09 09:53:41 -0300

    Efecto de Portal añadido a las naves
    Faltaría añadir musica al nivel

--- Assets/Scripts/Niveles/ShipLevelManager.cs
+++ Assets/Scripts/Niveles/ShipLevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShipLevelManager : MonoBehaviour
 {
@@ -8,9 +9,11 @@ public class ShipLevelManager : MonoBehaviour
     public GameObject Oleada1;
     public GameObject Oleada2;
     public GameObject Oleada3;
+    [SerializeField] private TextMeshProUGUI oleadaTMP;
     void Start()
     {
         Oleada1.SetActive(true);
+        oleadaTMP.text = "Oleada: 1";
         Oleada2.SetActive(false);
         Oleada3.SetActive(false);
     }
@@ -18,7 +21,17 @@ public class ShipLevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Enemies == 8) Oleada2.SetActive(true);
-        if (Enemies == 4) Oleada3.SetActive(false);
+        if (Enemies == 8) ActivarOleada2();
+        if (Enemies == 4) ActivarOleada3();
+    }
+    public void ActivarOleada2()
+    {
+        Oleada2.SetActive(true);
+        oleadaTMP.text = "Oleada: 2";
+    }
+    public void ActivarOleada3()
+    {
+        Oleada3.SetActive(true);
+        oleadaTMP.text = "Oleada: 3";
     }
 }
