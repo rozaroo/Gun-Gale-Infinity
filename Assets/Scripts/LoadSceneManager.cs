18d3fd5812418738cfa1f7b01d3546e84e1f0140
rozaroo <roramos@uade.edu.ar>
2024-10-27 16:03:45 -0300

    Escena de Carga añadida

---
+++ Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text countText;
+    private float maxTime;
+
+    void Start()
+    {
+        maxTime = 10.0f;
+    }
+
+    void Update()
+    {
+       if (maxTime > 0) maxTime -= Time.deltaTime;
+       if (maxTime == 0) SceneManager.LoadScene(4);
+       int seconds = Mathf.FloorToInt(maxTime%60);
+       countText.text = string.Format("{1:00}", seconds);
+    }
+}
