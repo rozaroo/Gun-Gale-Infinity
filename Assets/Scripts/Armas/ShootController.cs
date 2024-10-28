812d70ea1f4bb6a06bb62338789bc8ea76b91ea8
rozaroo <129896867+rozaroo@users.noreply.github.com>
2024-10-28 19:13:10 -0300

    Actualizar ShootController.cs

--- Assets/Scripts/Armas/ShootController.cs
+++ Assets/Scripts/Armas/ShootController.cs
@@ -31,12 +31,6 @@ public class ShootController : MonoBehaviour
             enemy.TakeDamage(shootDamage);
             Destroy(gameObject);
         }
-        var enemytwo = collision.collider.GetComponent<SpaceEnemyTwoController>();
-        if (enemytwo != null)
-        {
-            enemytwo.TakeDamage(shootDamage);
-            Destroy(gameObject);
-        }
     }
 }
 
