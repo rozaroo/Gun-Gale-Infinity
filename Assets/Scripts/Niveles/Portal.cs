8bb080da2728410b117719e96290f48f31d52c4d
rozaroo <129896867+rozaroo@users.noreply.github.com>
2024-10-28 23:38:07 -0300

    Actualizar Portal.cs

--- Assets/Scripts/Niveles/Portal.cs
+++ Assets/Scripts/Niveles/Portal.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    int nextSceneIndex = SceneManager.GetActiveScene()buildIndex + 1;
     public void OnCollisionEnter(Collision collision)
     {
         var player = collision.collider.GetComponent<PlayerController>();
-        if (player != null) SceneManager.LoadScene(nextSceneIndex);
+        if (player != null) 
+        {
+           int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;                 SceneManager.LoadScene(nextSceneIndex);
+        }
     }
 }
